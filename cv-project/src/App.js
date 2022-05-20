8e856384170ecc6779e5cd0ff5b4bb26a1c45fe3
corbincargil <corbin.cargil@gmail.com>
2022-05-20 00:09:08 -0500

    deleted task list exercise. added display and input components. created static site.

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,45 +1,23 @@
 import { Component } from 'react';
 import './App.css';
-import Overview from './components/Overview.js';
+import DisplayContainer from './components/DisplayContainer';
+import InputContainer from './components/InputContainer.js';
 
 class App extends Component {
   constructor(){
     super();
 
     this.state = {
-      newTask: 'Hi',
-      tasks: [],
+      
     };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.addNewTask = this.addNewTask.bind(this);
   }
 
-  handleChange(e) {
-    this.setState({ 
-      newTask: e.target.value,
-    });
-  }
-
-  addNewTask(e) {
-    e.preventDefault();
-    this.setState({
-      tasks: this.state.tasks.concat(this.state.newTask),
-    })
-  }
   
   render() {
       return (
       <div className="App">
-        <h1>Task List Exercise</h1>
-        <h2>Create Tasks Here:</h2>
-        <form onSubmit={this.addNewTask}>
-          <label htmlFor="getInupt">Task name:</label>
-          <input type="text" name="newTask" id="getTask" onChange={this.handleChange}/>
-          <button type="submit">Add</button>
-          <Overview tasks={this.state.tasks}/>
-        </form>
-        
+        <InputContainer />
+        <DisplayContainer />
       </div>
     );
   }
